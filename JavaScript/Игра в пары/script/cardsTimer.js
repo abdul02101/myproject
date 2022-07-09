a1f64607c4287c870632b8011a23e32962cae596
abdul_shamia <abdulabdulika@gmail.com>
2022-07-10 01:18:05 +0600

    Исправление таймера

--- JavaScript/Игра в пары/script/cardsTimer.js
+++ JavaScript/Игра в пары/script/cardsTimer.js
@@ -2,6 +2,12 @@ import { rebootGame } from "./cardsEndGame.js";
 
 function timerCards(timerMinute, timerSecond, timerCreate, amountCards, timer, cancelBtn){
   let timerId = setInterval(function () {
+    if(timerMinute == 0 && timerSecond == 0){
+      clearInterval(timerId);
+      rebootGame('Время вышло!', amountCards, timer, cancelBtn)
+    }
+
+    if(document.querySelector('.b-popup') || document.querySelector('.form').style.display === 'flex' ) clearInterval(timerId)
 
     if(timerSecond == 0){
       --timerMinute;
@@ -10,12 +16,7 @@ function timerCards(timerMinute, timerSecond, timerCreate, amountCards, timer, c
     --timerSecond;
 
 
-    if(timerMinute == 0 && timerSecond == 0){
-      clearInterval(timerId);
-      rebootGame('Время вышло!', amountCards, timer, cancelBtn)
-    }
 
-    if(document.querySelector('.b-popup') || document.querySelector('.form').style.display === 'flex' ) clearInterval(timerId)
 
     timerCreate.textContent = `${timerMinute}:${timerSecond}`
   }, 1000)
